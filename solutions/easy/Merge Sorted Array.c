54dd5d79b1ec559a5abef61c7c6edf23742a0dc2
NAZRULHAKIM03 <muhamadnazrulhakim03@gmail.com>
2026-07-30 11:38:42 +0800

    Add solution for Merge Sorted Array

---
+++ solutions/easy/Merge Sorted Array.c
@@ -0,0 +1,28 @@
+// Title: Merge Sorted Array
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/merge-sorted-array/
+
+void merge(int* nums1, int nums1Size, int m, int* 
+nums2, int nums2Size, int n) {
+    int p1 = m - 1;
+    int p2 = n - 1;
+    int target = m + n - 1;
+
+    while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[target] = nums1[p1];
+            p1--;
+        } else {
+            nums1[target] = nums2[p2];
+            p2--;
+        }
+        target--;
+    }
+
+    while (p2 >= 0) {
+        nums1[target] = nums2[p2];
+        p2--;
+        target--;
+    }
+}

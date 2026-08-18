71824ca44c15605a78119886dfe95dc359b386ab
NAZRUL <muhamadnazrulhakim03@gmail.com>
2026-08-18 12:54:20 +0800

    Add solution for Remove Element

---
+++ solutions/easy/Remove Element.c
@@ -0,0 +1,20 @@
+// Title: Remove Element
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/remove-element/
+
+int removeElement(int* nums, int numsSize, int val) {
+    
+    int write = 0;
+
+    for (int i=0; i<numsSize; i++)
+    {
+        if (nums[i] != val)
+    }
+        nums[write] = nums[i];
+        write++;
+
+    return write;
+        {
+        }
+}

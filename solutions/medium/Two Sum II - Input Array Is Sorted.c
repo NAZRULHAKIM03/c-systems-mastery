1417c4704f30fe297efd363947994ec54570d56e
NAZRUL <muhamadnazrulhakim03@gmail.com>
2026-08-23 12:56:55 +0800

    Update Two Sum II - Input Array Is Sorted.c

--- solutions/medium/Two Sum II - Input Array Is Sorted.c
+++ solutions/medium/Two Sum II - Input Array Is Sorted.c
@@ -6,33 +6,33 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize) {
 
-    int* result = (int*)malloc(2*sizeof(int));
-    *returnSize = 2;
+    int* result = (int*)malloc(2*sizeof(int));
+    *returnSize = 2;
 
-    while (left < right)
-    {
-        if(sum == target)
-    }
-        int sum = numbers[left] + numbers[right];
+    int left = 0;
+    int right = numbersSize - 1;
 
-        {
-            result[0] = left + 1;
-        }
-            result[1] = right + 1;
-        else if(sum > target)
-        {
-            right--;
-        }
-        else
-        {
-            left++;
-        }
-            return result;
+    while (left < right)
+    {
+        int sum = numbers[left] + numbers[right];
 
-    int left = 0;
-    int right = numbersSize - 1;
+        if(sum == target)
+        {
+            result[0] = left + 1;
+            result[1] = right + 1;
+            return result;
+        }
+        else if(sum > target)
+        {
+            right--;
+        }
+        else
+        {
+            left++;
+        }
+    }
 
-    return result;
+    return result;
 }

eff85ee856c7f2ed693f8e014b60adf6cd8e51fe
NAZRUL <muhamadnazrulhakim03@gmail.com>
2026-09-15 12:08:31 +0800

    Add solution for Power of Two

---
+++ solutions/easy/Power of Two.c
@@ -0,0 +1,10 @@
+// Title: Power of Two
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/power-of-two/
+
+bool isPowerOfTwo(int n) {
+
+    return n > 0 && (n & (n-1)) == 0;
+    
+}

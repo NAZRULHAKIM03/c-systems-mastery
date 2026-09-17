4ba1bd8214bf0fcaa9f7758c7063356567388b3d
NAZRUL <muhamadnazrulhakim03@gmail.com>
2026-09-17 12:28:13 +0800

    Add solution for Hamming Distance

---
+++ solutions/easy/Hamming Distance.c
@@ -0,0 +1,18 @@
+// Title: Hamming Distance
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/hamming-distance/
+
+int hammingDistance(int x, int y) {
+    
+    int diff = x ^ y;
+    int count = 0;
+
+    while(diff != 0)
+    {
+        count += diff & 1;
+    }
+        diff >>= 1;
+
+    return count;
+}

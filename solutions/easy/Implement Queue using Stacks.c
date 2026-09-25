75dd2418da8ef42b21cfe193ecfb592f0eb98833
NAZRUL <muhamadnazrulhakim03@gmail.com>
2026-09-25 11:57:08 +0800

    Update Implement Queue using Stacks.c

--- solutions/easy/Implement Queue using Stacks.c
+++ solutions/easy/Implement Queue using Stacks.c
@@ -3,55 +3,75 @@
             // Language: C
             // Link: https://leetcode.com/problems/implement-queue-using-stacks/
 
-    obj->in_stack[++obj->in_top] = x;
+#define MAX 100
+
+typedef struct {
+    int in_stack[MAX];
+    int in_top;
+    int out_stack[MAX];
+    int out_top;
+} MyQueue;
+
+
+MyQueue* myQueueCreate() {
+    MyQueue* q = (MyQueue*)malloc(sizeof(MyQueue));
+    q->in_top = -1;
+    q->out_top = -1;
+    
+    return q;
 }
 
-int myQueuePop(MyQueue* obj) {
-    
-    if(obj->out_top == -1)
-    {
-        while(obj->in_top != -1)
-        {
-            obj->out_stack[++obj->out_top] = obj->in_stack[obj->in_top--];
-        }
-    }
+void myQueuePush(MyQueue* obj, int x) {
 
-    return obj->out_stack[obj->out_top--];
+    obj->in_stack[++obj->in_top] = x;
 }
 
-int myQueuePeek(MyQueue* obj) {
+int myQueuePop(MyQueue* obj) {
+    
+    if(obj->out_top == -1)
+    {
+        while(obj->in_top != -1)
+        {
+            obj->out_stack[++obj->out_top] = obj->in_stack[obj->in_top--];
+        }
+    }
 
-    if(obj->out_top == -1)
-    {
-        while(obj->in_top != -1)
-        {
-            obj->out_stack[++obj->out_top] = obj->in_stack[obj->in_top--];
-        }
-    }
-
-    return obj->out_stack[obj->out_top];
+    return obj->out_stack[obj->out_top--];
 }
 
-bool myQueueEmpty(MyQueue* obj) {
-    
-    return (obj->in_top == -1 && obj->out_top == -1);
+int myQueuePeek(MyQueue* obj) {
+
+    if(obj->out_top == -1)
+    {
+        while(obj->in_top != -1)
+        {
+            obj->out_stack[++obj->out_top] = obj->in_stack[obj->in_top--];
+        }
+    }
+
+    return obj->out_stack[obj->out_top];
 }
 
-void myQueueFree(MyQueue* obj) {
+bool myQueueEmpty(MyQueue* obj) {
+    
+    return (obj->in_top == -1 && obj->out_top == -1);
+}
 
-    free(obj);
+void myQueueFree(MyQueue* obj) {
+
+    free(obj);
 }
 
 /**
- * Your MyQueue struct will be instantiated and called as such:
- * MyQueue* obj = myQueueCreate();
- * myQueuePush(obj, x);
- 
- * int param_2 = myQueuePop(obj);
- 
- * int param_3 = myQueuePeek(obj);
- 
- * bool param_4 = myQueueEmpty(obj);
- 
- * myQueueFree(obj);
+ * Your MyQueue struct will be instantiated and called as such:
+ * MyQueue* obj = myQueueCreate();
+ * myQueuePush(obj, x);
+ 
+ * int param_2 = myQueuePop(obj);
+ 
+ * int param_3 = myQueuePeek(obj);
+ 
+ * bool param_4 = myQueueEmpty(obj);
+ 
+ * myQueueFree(obj);
 */
